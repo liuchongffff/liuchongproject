8f2a604a8f8b0c7b3a9f7941f738693eefe22cdb
liuchongffff <liuchongff.student@sina.com>
2020-08-06 14:45:09 +0800

    add demo.py

--- testimport/game_round2.py
+++ testimport/game_round2.py
@@ -2,25 +2,21 @@
 打斗多个回合，谁最后血量最多
 """
 def fight():
-    my_hp = 0
-    energy_hp = 0
     mhp = 1000
-    mpower = 100
+    mpower = 120
     ehp = 1000
     epower = 100
     print(f"the game begin. ")
-    my_hp = mhp - epower
-    energy_hp = ehp - mpower
 
     while True:
-        if my_hp <=0:
+        mhp = mhp - epower
+        ehp = ehp - mpower
+        if mhp <=0:
             print(f"i lose the game. ")
             break
-        elif energy_hp <= 0:
+        elif ehp <= 0:
             print(f"the enemy lose the game. ")
             break
-        my_hp = my_hp - epower
-        energy_hp = energy_hp - mpower
 
 #调用
 fight()
